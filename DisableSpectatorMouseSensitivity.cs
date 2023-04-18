ce04f6604eff85eb331d2581e17591f5baf8dc19
Math0424 <Calculus0424@gmail.com>
2023-04-18 12:56:28 -0500

    Add project files.

---
+++ DisableSpectatorMouseSensitivity.cs
@@ -0,0 +1,15 @@
+﻿using VRage;
+using VRage.Plugins;
+
+namespace DisableSpectatorMouseSensitivity
+{
+    public class DisableSpectatorMouseSensitivity : IPlugin
+    {
+        public void Init(object gameInstance) { }
+        public void Update() {
+            if (MySpectator.Static != null)
+                MySpectator.Static.SpeedModeAngular = 1;
+        }
+        public void Dispose() { } 
+    }
+}
